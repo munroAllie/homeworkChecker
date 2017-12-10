1b23e07188d3eeb8226176a7d89381f8ea402860
sericet <johnvmunro@gmail.com>
2017-12-10 15:41:03 -0400

    Setups URL struture properly

--- src/app/components/addstudent/addstudent.component.spec.ts
+++ src/app/components/addstudent/addstudent.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddStudentComponent } from './add-student.component';
+import { AddstudentComponent } from './addstudent.component';
 
-describe('AddStudentComponent', () => {
-  let component: AddStudentComponent;
-  let fixture: ComponentFixture<AddStudentComponent>;
+describe('AddstudentComponent', () => {
+  let component: AddstudentComponent;
+  let fixture: ComponentFixture<AddstudentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddStudentComponent ]
+      declarations: [ AddstudentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddStudentComponent);
+    fixture = TestBed.createComponent(AddstudentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
